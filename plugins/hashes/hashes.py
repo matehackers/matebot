1399b50534e41adc1fa23da0a1e87641097e4ec8
Desobediente Civil <desci@riseup.net>
2017-10-26 04:38:48 -0200

    Corrigindo hashes disponíveis

--- plugins/hashes/hashes.py
+++ plugins/hashes/hashes.py
@@ -8,9 +8,10 @@ class hashes():
   def __init__(self):
     pass
   def get_hashes(self):
-    return ', '.join(sorted(hashlib.algorithms_available)).lower()
+    return ', '.join(sorted(hashlib.algorithms_guaranteed)).lower()
   def return_hash(self, algo, text):
-    if algo in hashlib.algorithms_available:
+    algo = algo.lower()
+    if algo in [testing.lower() for testing in hashlib.algorithms_guaranteed]:
       return 'hash %s de \'%s\':\n\n%s' % (algo, text, getattr(hashlib, algo, None)(text.encode('utf-8')).hexdigest())
     else:
       return u'Desculpe, estou rodando em um servidor sem suporte para \'%s\', ou \'%s\' não é um algoritmo.\n\nAlgoritmos suportados: %s' % (algo, algo, self.get_hashes())
