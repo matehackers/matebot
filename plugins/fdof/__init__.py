942d9ef5d943fbc10468e1cf8bd07400f094f92e
Iuri Guilherme <iuri@greatful.com.br>
2019-07-17 21:58:24 -0300

    Chegreat

--- plugins/fdof/__init__.py
+++ plugins/fdof/__init__.py
@@ -16,6 +16,9 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Avisa que chegou por primeiro na Fábrica do Futuro
+def cmd_chegreat(args):
+  return cmd_cheguei(args)
+
 def cmd_cheguei(args):
   response = u"Chegou chegando, desnorteando a Fábrica toda!"
   return {
