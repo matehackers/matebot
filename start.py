dc158e8d6ed1191aa75a1e1b3f4d655fb4c39aff
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2020-08-09 16:08:53 -0300

    Movendo para #44

--- start.py
+++ start.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
 ## If `./start.py` doesn't work for you, try `python3 start.py`.
-## Sugerido rodar com `pipenv run python start.py telepot matebot`
+## Sugerido rodar com `pipenv run python start.py flask matebot`
 
 import sys
 
 from matebot import bot
 
 if __name__ == "__main__":
-  mode = 'telepot'
-  ## Este é o arquivo config/.matebot.cfg
+  #mode = 'telepot'
+  mode = 'flask'
+  ## Este é o arquivo instance/.matebot.config.py
   config_file = 'matebot'
   ## TODO fazer validação de verdade
   if len(sys.argv) > 1:
@@ -18,19 +19,19 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
       config_file = sys.argv[2]
       print(
-        u"Usando arquivo de configuração instance/.%s.cfg"
+        u"Usando arquivo de configuração instance/.%s.config.py"
         % (config_file)
       )
     else:
       print(
-        u"Arquivo de configuração não informado, instance/.%s.cfg \
+        u"Arquivo de configuração não informado, instance/.%s.config.py \
         presumido"
         % (config_file)
       )
   else:
     print(u"Modo de operação não informado, %s presumido." % (mode))
     print(
-      u"Arquivo de configuração não informado, config/.%s.cfg presumido"
+      u"Arquivo de configuração não informado, instance/.%s.config.py presumido"
       % (config_file)
     )
   bot(mode, config_file)
