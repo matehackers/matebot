6de5726459f91c09fc3585c1c9f8711bbb4d2cf3
Iuri Guilherme <2027447+iuriguilherme@users.noreply.github.com>
2020-08-30 12:55:15 -0300

    Tentando forçar KeyboardInterrupt

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 ## App padrão se não for especificado nenhum no .env
-from matebot.ptb_matebot import app
+from matebot.ptb_matebot import (
+  app,
+  updaters,
+)
+
+if __name__ == '__main__':
+  try:
+    app.run(threaded=True)
+  except KeyboardInterrupt:
+    print(u"Stopping updaters...")
+    for updater in updaters:
+      updater.stop()
+    print(u"...done.")
+  except Exception as e:
+    print(u"Exception: {}".format(str(e)))
+    raise
